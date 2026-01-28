ffcf3f9e4d19b9dc10c19d448776d7d308fc1cb7
Murilo Rálio <muriloralio8@gmail.com>
2026-01-28 20:35:04 -0300

    Add files via upload
    Primeiro Commit do site

---
+++ Script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Scroll suave interno
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
