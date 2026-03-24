8482007b2af75b52ed9a59d4b634fa9b8a0f3572
Murilo Rálio <muriloralio8@gmail.com>
2026-03-23 22:43:41 -0300

    Add files via upload
    Feito o carossel do site

--- script.js
+++ script.js
@@ -10,3 +10,47 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     }
   });
 });
+
+//Carossel animação
+const carrossel = document.querySelector('.carrossel');
+const cards = document.querySelectorAll('.card');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let index = 0;
+let interval;
+
+function updateCarousel() {
+  carrossel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  index = (index + 1) % cards.length;
+  updateCarousel();
+}
+
+function prevSlide() {
+  index = (index - 1 + cards.length) % cards.length;
+  updateCarousel();
+}
+
+function startAutoPlay() {
+  interval = setInterval(nextSlide, 20000); // 30 segundos
+}
+
+function resetAutoPlay() {
+  clearInterval(interval);
+  startAutoPlay();
+}
+
+next.addEventListener('click', () => {
+  nextSlide();
+  resetAutoPlay();
+});
+
+prev.addEventListener('click', () => {
+  prevSlide();
+  resetAutoPlay();
+});
+
+startAutoPlay();
